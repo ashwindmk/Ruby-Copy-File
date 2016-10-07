6966e4f54a5300fb60872f547c72f8d43c84dd20
Ashwin Dinesh <ashwin.dinesh@mirraw.com>
2016-10-07 19:45:05 +0530

    Created Ruby Copy File project

---
+++ cut_paste_file.rb
@@ -0,0 +1,4 @@
+require 'fileutils'
+
+# copies folder1/test1.txt to folder2 as test2.txt
+FileUtils.cp('/home/ashwin/RubymineProjects/Ruby-Cut-Paste-File/folder1/test1.txt', '/home/ashwin/RubymineProjects/Ruby-Cut-Paste-File/folder2/test2.txt')
